0f883a851ebb90e4c57a0cdfc91babe3b86e4f92
Владимир <duvla@mail.ru>
2023-03-08 21:05:01 +1000

    ConnectionOfLibraries

--- src/main/java/com/skypro/recipe/service/impl/ValidationServiceImpl.java
+++ src/main/java/com/skypro/recipe/service/impl/ValidationServiceImpl.java
@@ -1,7 +1,8 @@
-package com.skypro.recipe.service;
+package com.skypro.recipe.service.impl;
 
 import com.skypro.recipe.model.Ingredient;
 import com.skypro.recipe.model.Recipe;
+import com.skypro.recipe.service.ValidationService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
